8d4a3a080150793cf95724e99af0c738e56e1ac3
Yash Panchal <46398416+panchal-yash@users.noreply.github.com>
2022-07-25 10:18:47 +0530

    Update ubuntu-20.04-install-psmdb.sh

--- ubuntu-20.04-install-psmdb.sh
+++ ubuntu-20.04-install-psmdb.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-
 apt-get update -y
 
 sudo apt install gnupg2 curl -y
 
 wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
 
-sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
+sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb -y
+
+apt-get update -y
 
 sudo percona-release setup ps80
 
-sudo apt install percona-server-server
-
-
-
-
+sudo apt install percona-server-server -y
